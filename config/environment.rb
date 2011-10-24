7c00f6743ea038ac6cd84fb379c1a4daf06bc2f1
Brooke Lestock <bnl2ja@virginia.edu>
2011-10-24 12:04:37 -0400

    Init commit

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+require File.dirname(__FILE__) + "/../lib/faster_require-0.9.2/lib/faster_require"
+
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+FirstApp::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'c0d67549a014459df3a89dbc1c1975d3b10d995f52f2b8ba2a14bd5e44b3b8c499bf3bbbf653acc2aa59b762ec3e83bdbff0b4b15a309dbfc6338dd6b75e242a'
